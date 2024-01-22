8f45c03fdab66b775fdc0d5fd452c5ab02e1eba7
Bhavik Panchal <32439746+bhavikpanchal28@users.noreply.github.com>
2024-01-22 16:22:04 -0500

    Update game.py

--- game.py
+++ game.py
@@ -66,6 +66,15 @@ class Player(pygame.sprite.Sprite):
         self.direction = "left"
         self.animation_count = 0
         self.fall_count = 0
+        self.jump_count = 0
+
+    def jump(self):
+        self.y_vel = -self.GRAVITY * 8
+        self.animation_count = 0
+        self.jump_count += 1
+        if self.jump_count == 1:
+            self.fall_count= 0
+        
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -94,7 +103,7 @@ class Player(pygame.sprite.Sprite):
     def landed(self):
         self.fall_count = 0
         self.y_vel = 0
-        self.jum_count = 0
+        self.jump_count = 0
 
     def hit_head(self):
         self.count = 0
@@ -213,6 +222,10 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player.jump_count < 2:
+                    player.jump()
 
         player.loop(FPS)
         handle_movement(player, floor)
